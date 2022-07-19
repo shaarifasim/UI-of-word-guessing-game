9687e16106e7508112b3a51c19d1908d8a2f1c4a
Shaarif Asim <84714602+shaarifasim@users.noreply.github.com>
2022-07-19 16:02:36 +0500

    Add files via upload

---
+++ wordguessing game/app.js
@@ -0,0 +1,104 @@
+// var inputData;
+// var inputData = document.getElementById("screen");
+
+// function getValueA(){
+//     var button = document.getElementById('buttonA').value;
+//     inputData.innerHTML = button
+// }
+// function getValueB(){
+//     var value = B;
+//     screen(value);
+// }
+// function getValueC(){
+//     var value = C;
+//     screen(value);
+// }
+// function getValueC(){
+//     var value = C;
+//     screen(value);
+// }
+// function getValueD(){
+//     var value = D;
+//     screen(value);
+// }
+// function getValueE(){
+//     var value = E;
+//     screen(value);
+// }
+
+// function screen(v) {
+//     // if (v === "=") {
+//         var data = inputData.innerText;
+//         // var total = eval(data);
+        
+//         // document.getElementById("screen").innerText = total;
+//     } 
+//     // else {
+//         // document.getElementById("screen").innerText += v;
+//     // }
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+// // function getValue7(){
+// //     var value = 7;
+// //     screen(value);
+// // }
+
+
+
+
+
+
+
+
+
